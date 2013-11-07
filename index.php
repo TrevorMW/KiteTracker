71dfde0ecc8f0f2c3778b95555e56ed70c6139a0
Trevor <tmwagner66@gmail.com>
2013-11-06 22:12:13 -0500

    Adding Core Files

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Triage Practice</title>
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+<?php include('functions.php');?>
+
+
+
+ 
+hello
+</body>
+</html>
