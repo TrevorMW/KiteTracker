6b10965f7779b6031d9d53baa50339b68b1fb167
Trevor <tmwagner66@gmail.com>
2013-11-20 20:54:21 -0500

    Added map functionality to Form/Cache browser coordinates in local storage

--- recentSightings.php
+++ recentSightings.php
@@ -32,8 +32,17 @@
 		</div>
 	</div>
 
-	
+
 <script type="text/javascript">
+
+
+function supports_html5_storage() {
+    try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+        return false;
+    }
+}
 
 // CHANGE ZIP CODE AFTER INITIAL RENDER OF RESULTS
 jQuery('#changeZip').submit(function(event){
@@ -64,38 +73,45 @@ function get_location() {
 	var coordinates = navigator.geolocation.getCurrentPosition(show_map, handle_error);
 }
 
+
+
 // GET COORDINATES IF USER ALLOWS GEOLOCATION THROUGH BROWSER
 function show_map(position) {
     // GET LATITUTDE FROM BROWSER
  	var latitude = position.coords.latitude;
- 	// GET LONGITUTDE FROM BROWSER 
- 	var longitude = position.coords.longitude; 
- 	var tRlat = latitude + 0.25;
+ 	// GET LONGITUDE FROM BROWSER
+ 	var longitude = position.coords.longitude;
+    var storage = supports_html5_storage();
+    if(latitude && longitude && storage == true){
+        localStorage.setItem('latitude', latitude);
+        localStorage.setItem('longitude', longitude);
+    }
+    var tRlat = latitude + 0.25;
  	var bLlat = latitude - 0.25;
  	var tRlng = longitude + 0.25;
  	var bLlng = longitude - 0.25;
  	// NATIVE JSON OF LAT & LONG & CALCULATED BOUNDARIES
-	var coords = {'lat':position.coords.latitude,'long':position.coords.longitude, 'tRlat':tRlat, 'tRlng':tRlng, 'bLlat':bLlat, 'bLlng':bLlng}; 
-	// IF JSON EXISTS, INITALIZE THE MAP WITH CORRECT COORDINATES AS MAP CENTER, ELSE RUN MODAL TO GRAB ZIP CODE	
-	if(coords){  
-		initialize(coords);  
-	} else { 
-		handle_error();  
-	}	
+	var coords = {'lat':position.coords.latitude,'long':position.coords.longitude, 'tRlat':tRlat, 'tRlng':tRlng, 'bLlat':bLlat, 'bLlng':bLlng};
+	// IF JSON EXISTS, INITALIZE THE MAP WITH CORRECT COORDINATES AS MAP CENTER, ELSE RUN MODAL TO GRAB ZIP CODE
+	if(coords){
+		initialize(coords);
+	} else {
+		handle_error();
+	}
 }
 
-// IF USER DENIES ACCESS TO BROWSER GEOLOCATION, ASK FOR ZIPCODE	
-function handle_error(err) {		
-	if (err.code == 1) { 
+// IF USER DENIES ACCESS TO BROWSER GEOLOCATION, ASK FOR ZIPCODE
+function handle_error(err) {
+	if (err.code == 1) {
   	// IF USER DENIES ACCESS TO LOCATION, SHOW ZIP CODE MODAL
 	jQuery('#zipcode').modal('show');
 	// FIND FORM
 	var form = jQuery('#zipForm');
 		// CAPTURE SUBMIT & PREVENT REAL POST SUBMISSION
 		form.submit(function(event){
-			event.preventDefault();	
-			// FIND VALUE IN ZIP FORM    	
-			var zip = form.find('input[type="text"]').val(); 
+			event.preventDefault();
+			// FIND VALUE IN ZIP FORM
+			var zip = form.find('input[type="text"]').val();
 			if(zip == ''){
 				// IF NO VALUE FOR ZIP CODE, THROW ERROR
 				zip_error();
@@ -103,7 +119,7 @@ function handle_error(err) {
 				// REMOVE ERROR BOX IF THERE, AND PROCEED
 				jQuery('#zipError').remove();
 				// SEND THIS ZIP TO GOOGLE GEOCODING FUNCTION
-				geocode_zip(zip);	
+				geocode_zip(zip);
 			}
 		return false;
 		});
@@ -118,24 +134,29 @@ function geocode_zip(zip){
 	var geocoder;
     	geocoder = new google.maps.Geocoder();
 	// START GEOCODING THE ZIPCODE
-	geocoder.geocode( { 'address': zip}, function(results, status) { 
-	
-		if (status == google.maps.GeocoderStatus.OK) {   
-			
+	geocoder.geocode( { 'address': zip}, function(results, status) {
+
+		if (status == google.maps.GeocoderStatus.OK) {
+
 			// BUILD BOUNDARIES OBJECT TO SEND VIA AJAX TO GRAB NEARBY SIGHTINGS
-			var topRight = results[0].geometry.viewport.getNorthEast(); 
+			var topRight = results[0].geometry.viewport.getNorthEast();
 			var bottomLeft = results[0].geometry.viewport.getSouthWest();
-			
-			console.log(results[0].geometry.viewport);							
-													
-			// DEFINE COORDINATES OBJECT WITH LAT AND LONG 
+
+            var lat = results[0].geometry.location.lat();
+            var lng = results[0].geometry.location.lng();
+            var storage = supports_html5_storage();
+            if( lat && lng && storage == true){
+                localStorage.setItem('latitude', lat);
+                localStorage.setItem('longitude', lng);
+            }
+            // DEFINE COORDINATES OBJECT WITH LAT AND LONG
 			var coords = {'lat':results[0].geometry.location.lat(),
-						  'long':results[0].geometry.location.lng(), 
+						  'long':results[0].geometry.location.lng(),
 						  'tRlat':topRight.lat() + .25,
-						  'tRlng':topRight.lng()+ .25, 
+						  'tRlng':topRight.lng()+ .25,
 						  'bLlat':bottomLeft.lat()- .25,
 						  'bLlng':bottomLeft.lng()-  .25}
-						  
+
 			// HIDE MODAL BEFORE INITIALIZING NEW MAP
 			jQuery('#zipcode').modal('hide');
 		} else {
@@ -143,44 +164,44 @@ function geocode_zip(zip){
 			var coords = {'lat':0,
 						  'long':0,
 						  'topRightLat':40,
-						  'topRightLng':-78, 
+						  'topRightLng':-78,
 						  'bottomLeftLat':30,
 						  'bottomLeftLng':-87};
 		}
 		// INITALIZE THE MAP WITH ZIP CODE COORDINATES
 		initialize(coords);
-		
+
 	});
-} 
+}
 
 // INITIALIZE GOOGLE MAPS WITH APPLICABLE PARAMETERS
-function initialize(coords) { 
+function initialize(coords) {
 	// HIDE ANY PREVIOUS MESSAGES
 	jQuery('#resultsMsg').html('').hide();
 
    	// TURN JSON COORDINATE OBJECT INTO PARAMETERS TO SEND TO AJAX
-    var serializedCoords = jQuery.param(coords); 
-      	
+    var serializedCoords = jQuery.param(coords);
+
   	// DEFINE CERTAIN VARIABLES USED TO BUILD PAGE STRUCTURE
-	var trueHeight = jQuery(window).height() - jQuery('.header nav').height(); 
+	var trueHeight = jQuery(window).height() - jQuery('.header nav').height();
 	var mapBox = jQuery('#map-canvas');
 	var leftCol = jQuery('.leftCol');
-	
-	// ADD HEIGHT OF PAGE TO MAP			 
+
+	// ADD HEIGHT OF PAGE TO MAP
 	mapBox.css('height', trueHeight);
-    
-    // SET MAP OPTIONS DEPENDING ON GEOLOCATION METHOD      
+
+    // SET MAP OPTIONS DEPENDING ON GEOLOCATION METHOD
     var mapOptions = {
        center: new google.maps.LatLng(coords.lat, coords.long),
        zoom: 10,
        mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    
+
 	var bLlng = coords.bLlng - 0.20;
 	var bLlat = coords.bLlat - 0.20;
 	var tRlat = coords.tRlat + 0.20;
 	var tRlng = coords.tRlng + 0.20;
-	
+
 	/* var flightPlanCoordinates = [
 		new google.maps.LatLng(tRlat,bLlng),
 		new google.maps.LatLng(tRlat,tRlng),
@@ -188,7 +209,7 @@ function initialize(coords) {
 		new google.maps.LatLng(bLlat,bLlng),
 		new google.maps.LatLng(tRlat,bLlng)
 	];
-		
+
 	var flightPath = new google.maps.Polyline({
 		path: flightPlanCoordinates,
 		geodesic: true,
@@ -198,31 +219,37 @@ function initialize(coords) {
 		fillColor: '#FF0000',
 		fillOpacity: 0.35,
 	});  IF BOUNDS AREA NEEDS TO BE PHYSICALLY SEEN, FUNCTION CAN DRAW RECTANGLE FROM POINTS */
-	        
+
     // BUILD MAP WITH WHATEVER CURRENT VARIABLES AND DATABASE INFORMATION IS PRESENT
     var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
-    
-    //flightPath.setMap(map);	
-    
-    // WITH JSON PARAMS, CALL FOR SIGHTINGS WITHIN THE VIEWPORT DEFINED BY BROWSER LOCATION OR ZIP CODE 
-    jQuery.ajax({
-	    url: "assets/scripts/findSightings.php",
-	    dataType:'json', 
-	    data: serializedCoords,
-		success: function(data){
-			// IF DATA IS RETURNED, BEGIN PARSING IT AND ADDING MARKERS TO THE MAP
-			if(data){ 		
-				jQuery('#resultsMsg').append('<p>We found '+data.length+' sightings in your area.</p>').addClass('alert-success').slideDown();
-				build_sightings_list(map, data);
-			}	
-		}
-    });
+
+    //flightPath.setMap(map);
+
+    find_sightings_records(map, serializedCoords);
+
 } // END INITALIZE FUNCTION
 
 google.maps.event.addDomListener(window, 'load', initialize);
-	
+
+function find_sightings_records(map, serializedCoords){
+// WITH JSON PARAMS, CALL FOR SIGHTINGS WITHIN THE VIEWPORT DEFINED BY BROWSER LOCATION OR ZIP CODE
+jQuery.ajax({
+    url: "assets/scripts/findSightings.php",
+    dataType:'json',
+    data: serializedCoords,
+    success: function(data){
+        // IF DATA IS RETURNED, BEGIN PARSING IT AND ADDING MARKERS TO THE MAP
+        if(data){
+            jQuery('#resultsMsg').append('<p>We found '+data.length+' sightings in your area.</p>').addClass('alert-success').slideDown();
+            build_sightings_list(map, data);
+        }
+    }
+});
+
+}
 function add_map_markers(map, id, year, species_id, lat, lng) {
-	var linkId = id; console.log(id);
+
+    var linkId = id;
 	// BUILD MARKUP FOR EACH MARKER INFOWINDOW
 	var contentString = '<div class="google-info-window" id="info-window">'+'<h3>'+ species_id +'</h3>'+'<div class="info-window-content">'+
 	  '<h5>'+id+'</h5> '+'<p>(Data recorded in '+year+').</p>'+'</div>'+'</div>';
@@ -232,8 +259,11 @@ function add_map_markers(map, id, year, species_id, lat, lng) {
 	   id: linkId,
 	   map: map,
 	   animation:google.maps.Animation.DROP
-	});	
-	// DEFINE INFO WINDOW PARAMETERS     
+	});
+
+
+
+	// DEFINE INFO WINDOW PARAMETERS
 	var infowindow = new google.maps.InfoWindow({
 	  content: contentString
 	});
@@ -243,27 +273,26 @@ function add_map_markers(map, id, year, species_id, lat, lng) {
 		// sideLink.addClass('active').find('a').css('font-size','red');
 		infowindow.open(map, marker);
 	});
-		
+
 }
 
-function build_sightings_list(map,data){ 
-	// LOOP THROUGH DATA TO BUILD SIDEBAR LIST			
-    for ( var i = 0; i < data.length; i++) {
-    	// BUILD LI LIST
-    	var sightingsHTML = '<li id="'+data[i].Id+'"><a href="#" onclick=""><h4>'+data[i].species_id+'</h4></a></li>';
-    	// APPEND LIST TO UL CONTAINER
-    	jQuery('#sightingsPanel').append(sightingsHTML);
-    	// DEFINE VARIABLES NECESSARY TO BUILD MARKERS
-    	var id = data[i].Id;
-    	var year = data[i].year;
-    	var species_id = data[i].species_id;
-    	var lat = data[i].lat;
-    	var lng = data[i].lng;
-		// SEND APPROPRIATE DATA TO BUILD MAP MARKERS
-		add_map_markers(map, id, year, species_id, lat, lng);
-	
-	}	
+function build_sightings_list(map,data){
+	// LOOP THROUGH DATA TO BUILD SIDEBAR LIST
+    for ( var j = 0; j < data.length; j++) {
+
+        var sightingsHTML = '<li id="'+data[j].Id+'"><a href="#" onclick=""><h4>'+data[j].species_id+'</h4></a></li>';
+        jQuery('#sightingsPanel').append(sightingsHTML);
+
+    	var id = data[j].Id;
+    	var year = data[j].year;
+    	var species_id = data[j].species_id;
+    	var lat = data[j].lat;
+    	var lng = data[j].lng;
+
+        add_map_markers(map, id, year, species_id, lat, lng);
+    }
 }
 </script>
+
 
 <?php include('assets/inc/footer.php'); ?>
--- reportSighting.php
+++ reportSighting.php
@@ -1,12 +1,12 @@
 <?php include('assets/inc/header.php'); include('functions.php'); $currentMonth = date('F');   ?>
-
+<div class="wrapper alert alert-info" id="formMsg"></div>
 <div class="wrapper sighting-form" style="padding:15px 0;">
 
     <div class="col-lg-3">
 
-        <form id="sightingWhen" action="" method="post" class="">
+        <form id="sightingWhen" action="" method="post" class="one" data-form-number="1">
 
-            <fieldset class="panel panel-default">
+            <fieldset class="panel panel-default open">
 
             <header class="panel-heading">When did this sighting occur?</header>
 
@@ -34,7 +34,7 @@
             }?> </select></li>
 
             <li class="half"><label>Time:</label>
-            <select name="year">
+            <select name="sightingTime">
             <?php  for($m = 1;$m <= 12; $m++){
                 echo "<option value='$m'>$m:00 am</option>";
             }
@@ -45,8 +45,9 @@
             </ul>
 
             <footer class="panel-footer">
-                <div class="btn-group ">
+                <div class="btn-group">
                     <button class="save btn btn-default btn-xs" type="submit">Save</button>
+                    <button class="edit btn btn-default btn-xs" style="display:none;">Edit</button>
                 </div>
             </footer>
 
@@ -54,7 +55,7 @@
 
         </form>
 
-        <form id="sightingsObservation" class="" action="" method="">
+        <form id="sightingsObservation" class="two" action="" method="post" data-form-number="3">
 
         <fieldset class="panel panel-default">
 
@@ -62,23 +63,39 @@
 
             <ul class="panel-body">
 
-                <li></li>
+                <li>
+                    <label>Number of Kites seen:</label>
+                    <select name="numberSpotted">
+                    <?php  for($m = 1;$m <= 20; $m++){
+                        echo "<option value='$m'>$m</option>";
+                    } ?>
+                    </select>
+                </li>
 
-                <li></li>
+                <li>
+                   <label>Was a nest observed?</label>
+                    <ul class="multi">
+                        <li><input type="radio" name="nestObserved" value="0" /><span>Yes</span></li>
+                        <li><input type="radio" name="nestObserved" value="1" /><span>No</span></li>
+                    </ul>
+                </li>
 
-                <li></li>
+                <li>
+                    <label></label>
+
+                    <ul class="multi">
+                    <li><input type="checkbox" name="vehicle" value="Bike"><span>I have a bike</span></li>
+                    <li><input type="checkbox" name="vehicle" value="Car"><span>I have a car</span></li>
+                    </ul>
+                </li>
 
             </ul>
 
-            <footer class="panel-footer ">
-
+            <footer class="panel-footer">
                 <div class="btn-group">
-
-                     <button class="save btn btn-default btn-xs" type="submit">Save</button>
-                    <button class="edit btn btn-default btn-xs" type="submit">Save</button>
-
+                    <button class="save btn btn-default btn-xs" type="submit">Save</button>
+                    <button class="edit btn btn-default btn-xs" style="display:none;">Edit</button>
                 </div>
-
             </footer>
 
         </fieldset>
@@ -89,28 +106,205 @@
 
     <div class="col-lg-9">
 
-        <form id="sightingLocation" class="" action="" method="">
+        <form id="sightingLocation" class="" action="" method="post" data-form-number="2">
 
             <fieldset class="panel panel-default">
 
-                <header class="panel-heading">Location:</header>
+                <header class="panel-heading">Location:
 
-                <ul class="panel-body">
+                    <form id="" class="pull-right" style="width:300px; border:1px solid red; height:35px" action="" method="">
 
 
+                    </form>
 
-                </ul>
+                </header>
+
+                 <div class="panel-body">
+
+                     <ul class="col-lg-4">
+
+                      <li class="half">
+                          <label>State:</label>
+                          <select name="state">
+
+                              <option value="Alabama">
+                                  Alabama
+                              </option>
+                              <option value="Alaska">
+                                  Alaska
+                              </option>
+                              <option value="Arizona">
+                                  Arizona
+                              </option>
+                              <option value="Arkansas">
+                                  Arkansas
+                              </option>
+                              <option value="California">
+                                  California
+                              </option>
+                              <option value="Colorado">
+                                  Colorado
+                              </option>
+                              <option value="Connecticut">
+                                  Connecticut
+                              </option>
+                              <option value="Delaware">
+                                  Delaware
+                              </option>
+                              <option value="Florida">
+                                  Florida
+                              </option>
+                              <option value="Georgia">
+                                  Georgia
+                              </option>
+                              <option value="Hawaii">
+                                  Hawaii
+                              </option>
+                              <option value="Idaho">
+                                  Idaho
+                              </option>
+                              <option value="Illinois">
+                                  Illinois
+                              </option>
+                              <option value="Indiana">
+                                  Indiana
+                              </option>
+                              <option value="Iowa">
+                                  Iowa
+                              </option>
+                              <option value="Kansas">
+                                  Kansas
+                              </option>
+                              <option value="Kentucky">
+                                  Kentucky
+                              </option>
+                              <option value="Louisiana">
+                                  Louisiana
+                              </option>
+                              <option value="Maine">
+                                  Maine
+                              </option>
+                              <option value="Maryland">
+                                  Maryland
+                              </option>
+                              <option value="Massachusetts">
+                                  Massachusetts
+                              </option>
+                              <option value="Michigan">
+                                  Michigan
+                              </option>
+                              <option value="Minnesota">
+                                  Minnesota
+                              </option>
+                              <option value="Mississippi">
+                                  Mississippi
+                              </option>
+                              <option value="Missouri">
+                                  Missouri
+                              </option>
+                              <option value="Montana">
+                                  Montana
+                              </option>
+                              <option value="Nebraska">
+                                  Nebraska
+                              </option>
+                              <option value="Nevada">
+                                  Nevada
+                              </option>
+                              <option value="New Hampshire">
+                                  New Hampshire
+                              </option>
+                              <option value="New Jersey">
+                                  New Jersey
+                              </option>
+                              <option value="New Mexico">
+                                  New Mexico
+                              </option>
+                              <option value="New York">
+                                  New York
+                              </option>
+                              <option value="North Carolina">
+                                  North Carolina
+                              </option>
+                              <option value="North Dakota">
+                                  North Dakota
+                              </option>
+                              <option value="Ohio">
+                                  Ohio
+                              </option>
+                              <option value="Oklahoma">
+                                  Oklahoma
+                              </option>
+                              <option value="Oregon">
+                                  Oregon
+                              </option>
+                              <option value="Pennsylvania">
+                                  Pennsylvania
+                              </option>
+                              <option value="Rhode Island">
+                                  Rhode Island
+                              </option>
+                              <option value="South Carolina">
+                                  South Carolina
+                              </option>
+                              <option value="South Dakota">
+                                  South Dakota
+                              </option>
+                              <option value="Tennessee">
+                                  Tennessee
+                              </option>
+                              <option value="Texas">
+                                  Texas
+                              </option>
+                              <option value="Utah">
+                                  Utah
+                              </option>
+                              <option value="Vermont">
+                                  Vermont
+                              </option>
+                              <option value="Virginia">
+                                  Virginia
+                              </option>
+                              <option value="Washington">
+                                  Washington
+                              </option>
+                              <option value="West Virginia">
+                                  West Virginia
+                              </option>
+                              <option value="Wisconsin">
+                                  Wisconsin
+                              </option>
+                              <option value="Wyoming">
+                                  Wyoming
+                              </option>
+                          </select>
+                      </li>
+
+                      <li class="half">
+                          <label>County:</label>
+                          <input type="text" id="" class="" placeholder="" />
+                      </li>
+
+                       <li class="full">
+                           <label></label>
+
+                       </li>
+
+                      </ul>
+
+                     <ul class="map col-lg-8">
+
+                         <div class="" id="map-canvas" style="border:1px solid #ccc;height:300px"></div>
+
+                     </ul>
+
+                 </div>
 
                 <footer class="panel-footer">
-
-                    <ul>
-
-                        <li><button class="btn btn-default btn-xs" type="submit">Save</button></li>
-
-                        <li><button class="btn btn-default btn-xs" type="submit">Save</button></li>
-
-                    </ul>
-
+                    <div class="">
+                        <button class="save btn btn-default btn-xs" type="submit">Save</button>
+                        <button class="edit btn btn-default btn-xs" style="display:none;">Edit</button>
+                    </div>
                 </footer>
 
             </fieldset>
@@ -122,6 +316,9 @@
 </div>
 
 <script>
+
+    jQuery('#formMsg').hide();
+
     function supports_html5_storage() {
         try {
             return 'localStorage' in window && window['localStorage'] !== null;
@@ -130,29 +327,96 @@
         }
     }
 
-    jQuery('#sightingWhen').submit(function(event){
+    // ON SAVE BUTTON CLICK, FIND NEAREST FORM, THEN BEGIN VALIDATION
+    jQuery(document).on('click', 'button.save', function(event){
+        event.preventDefault();
+        var form = jQuery(this).closest('form');
+        var btn = jQuery(this);
+        var formNumber = form.attr('data-form-number');
+        if(form.hasClass('done') == false){
+            var formData = form.serializeArray();
+            var storage = supports_html5_storage();
+            manage_form_data(form, btn, formNumber, formData, storage);
+        }
+
+    });
+
+    function manage_form_data(form, btn, formNumber, formData, storage){
+    if(true){
+        if(storage == true){
+            for(var i = 0; i < formData.length; i++ ){
+                localStorage.setItem(JSON.stringify(formData[i].name), JSON.stringify(formData[i].value));
+            };
+            form.addClass('done').find('.panel-body').slideUp('slow', function(){
+                form.find('fieldset').addClass('panel-success');
+                form.find('button.edit').show();
+                btn.removeClass('btn-default').addClass('btn-success').text('Saved!');
+            });
+
+           var formNumber = parseInt(formNumber) + 1;
+           var nextForm =  jQuery(document).find('[data-form-number="'+formNumber+'"]');
+           nextForm.find('.panel-body').slideDown('slow', function(){
+               nextForm.find('footer button.save').show();
+               initialize();
+           });
+        } else {
+            cookie_storage(formData);
+        }
+    }
+    }
+
+
+    /*jQuery('form').submit(function(event){
         event.preventDefault();
         var form = jQuery(this);
         var formData = form.serialize();
         var formData = JSON.stringify(formData);
-        var storage = supports_html5_storage();
-        if(storage == true){
-            form.find('ul.panel-body').slideToggle();
-            form.find('fieldset.panel').toggleClass('panel-default').toggleClass('panel-success');
-            var formbutton = form.find('footer button');
-            if(formbutton.length <= 1){
-                form.find('footer.panel-footer').append('<button class="edit btn btn-default btn-xs">Edit</button>');
-                form.find('footer button.save').html('Saved').toggleClass('btn-default').toggleClass('btn-success');
-            } else {
-                form.find('footer button.edit').hide();
-                form.find('footer button.save').html('Save').toggleClass('btn-success').toggleClass('btn-default');
-            }
-
-        } else {
-
+        var editBtn = form.find('footer button.edit');
+        var panelBody = form.find('ul.panel-body')
+        if(panelBody.hasClass('open')){
+            panelBody.slideUp('slow').removeClass('open');
         }
-        console.log(storage, formData);
-    });
+        form.find('fieldset.panel').toggleClass('panel-default').toggleClass('panel-success');
+        var formbutton = form.find('footer button.save');
+        editBtn.show();
+        formbutton.html('Saved').toggleClass('btn-default').toggleClass('btn-success').attr('disabled','disabled');
+
+         jQuery(document).on('click',editBtn, function(){
+            form.find('ul.panel-body').slideDown('slow',function(){
+                formbutton.removeAttr('disabled').html('Save').addClass('btn-default').removeClass('btn-success');
+                panelBody.addClass('open');
+                editBtn.hide();
+            })
+         });
+
+        console.log(formData);
+    });*/
+
+    function initialize() {
+        var lat = localStorage.getItem('latitude');
+        var lng = localStorage.getItem('longitude');
+        var coords = new google.maps.LatLng(lat, lng);
+        jQuery('#').val(lat);
+        jQuery('#').val(lng);
+        var mapOptions = {
+            center: coords,
+            zoom: 12
+        };
+
+        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+        var marker = new google.maps.Marker({
+            position:coords,
+            map:map,
+            animation: google.maps.Animation.DROP,
+            draggable:true,
+            title:'Draggable Coordinate Picker'
+        });
+
+
+
+    }
+
 
 
 </script>
